7d006fe2428846b00eec25a8444796fd070d7c6b
dlfuqsla <dlfduqzzang@nate.com>
2022-02-04 21:47:11 +0900

    자식 브랜치 commit

--- MainProject/src/main/java/music/is/mylife/dao/SongDAO.java
+++ MainProject/src/main/java/music/is/mylife/dao/SongDAO.java
@@ -10,7 +10,19 @@ public class SongDAO {
 	@Autowired
 	private SqlSession session;
 	
-	public void func1() {
-		System.
+	public void func3() {
+		System.out.println("권윤영");
+		System.out.println("is");
+		System.out.println("바보");
+	}
+	public void func4() {
+		System.out.println("전진");
+		System.out.println("is");
+		System.out.println("아이디어뱅크");
+	}
+	public void func5() {
+		System.out.println("박화원");
+		System.out.println("is");
+		System.out.println("권윤영 담당 일진");
 	}
 }
